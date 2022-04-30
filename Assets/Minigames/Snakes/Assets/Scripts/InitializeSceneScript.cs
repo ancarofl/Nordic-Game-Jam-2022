a961e5600477d86eb33460d8d8c510c912c165bb
Anca N <negulescuanca11@gmail.com>
2022-04-30 22:19:36 +0200

    Create outline prefabs; select previous or next snake; show outline around selected snake

--- Assets/Minigames/Snakes/Assets/Scripts/InitializeSceneScript.cs
+++ Assets/Minigames/Snakes/Assets/Scripts/InitializeSceneScript.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class InitializeSceneScript : MonoBehaviour
 {
+    // Prefabs for snake types and snake glows by type
     public GameObject downLeftHorizontalSnakePrefab;
+    public GameObject downLeftHorizontalSnakeGlowPrefab;
+
     public GameObject downLeftVerticalSnakePrefab;
+    public GameObject downLeftVerticalSnakeGlowPrefab;
+
     public GameObject downRightHorizontalSnakePrefab;
+    public GameObject downRightHorizontalSnakeGlowPrefab;
+
     public GameObject downRightVerticalSnakePrefab;
+    public GameObject downRightVerticalSnakeGlowPrefab;
+
     public GameObject upLeftHorizontalSnakePrefab;
+    public GameObject upLeftHorizontalSnakeGlowPrefab;
+
     public GameObject upLeftVerticalSnakePrefab;
+    public GameObject upLeftVerticalSnakeGlowPrefab;
+
     public GameObject upRightHorizontalSnakePrefab;
+    public GameObject upRightHorizontalSnakeGlowPrefab;
+
     public GameObject upRightVerticalSnakePrefab;
+    public GameObject upRightVerticalSnakeGlowPrefab;
+
 
     public int snakeNumber;
-    public GameObject selectedSnake;
 
     private GameObject[] snakes;
+    private GameObject selectedSnake;
+    private GameObject selectedSnakeGlow;
+    private int selectedSnakeIndex;
 
     void Awake()
     {
@@ -41,11 +60,15 @@ public class InitializeSceneScript : MonoBehaviour
         if (controls.Gameplay.Action1.triggered)
         {
             Debug.Log("Q => Kill");
+
+            SelectPreviousSnake();
         }
         // E
         else if (controls.Gameplay.Action2.triggered)
         {
             Debug.Log("E => Pat");
+
+            SelectNextSnake();
         }
         // F. Change this to Action3 (R) in case the other minigames only use 3 buttons.
         else if (controls.Gameplay.Action4.triggered)
@@ -54,14 +77,106 @@ public class InitializeSceneScript : MonoBehaviour
         }
     }
 
+    void SelectPreviousSnake()
+    {
+        // If not first element of the array: index = previous index
+        if (selectedSnakeIndex > 0)
+        {
+            selectedSnakeIndex--;
+        }
+        // Otherwise: index = last index
+        else
+        {
+            selectedSnakeIndex = snakeNumber - 1;
+        }
+        selectedSnake = snakes[selectedSnakeIndex];
+        OutlineSnake();
+    }
+
+
+
+    void SelectNextSnake()
+    {
+        // If not last element of the array: index = next index
+        if (selectedSnakeIndex < snakeNumber - 1)
+        {
+            selectedSnakeIndex++;
+        }
+        // Otherwise: index = first index
+        else
+        {
+            selectedSnakeIndex = 0;
+        }
+        selectedSnake = snakes[selectedSnakeIndex];
+        OutlineSnake();
+    }
+    void OutlineSnake()
+    {
+        // Remove old outline
+        Destroy(selectedSnakeGlow);
+
+        // Instantiate current outline
+        if (selectedSnake.transform.CompareTag("DLH"))
+        {
+            Debug.Log("DLH");
+            selectedSnakeGlow = Instantiate(downLeftHorizontalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x + 0.05f, selectedSnake.transform.position.y - 0.001f);
+        }
+        else if (selectedSnake.transform.CompareTag("DLV"))
+        {
+            Debug.Log("DLV");
+            selectedSnakeGlow = Instantiate(downLeftVerticalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x - 0.01f, selectedSnake.transform.position.y - 0.026f); ;
+        }
+        else if (selectedSnake.transform.CompareTag("DRH"))
+        {
+            Debug.Log("DRH");
+            selectedSnakeGlow = Instantiate(downRightHorizontalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x + 2.729289f, selectedSnake.transform.position.y + 0.2612587f);
+        }
+        else if (selectedSnake.transform.CompareTag("DRV"))
+        {
+            Debug.Log("DRV");
+            selectedSnakeGlow = Instantiate(downRightVerticalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x + 0.037f, selectedSnake.transform.position.y - 0.018f);
+        }
+        else if (selectedSnake.transform.CompareTag("ULH"))
+        {
+            Debug.Log("ULH");
+            selectedSnakeGlow = Instantiate(upLeftHorizontalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x + 0.05f, selectedSnake.transform.position.y - 0.01f);
+        }
+        else if (selectedSnake.transform.CompareTag("ULV"))
+        {
+            Debug.Log("ULV");
+            selectedSnakeGlow = Instantiate(upLeftVerticalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x - 0.01f, selectedSnake.transform.position.y + 0.037f);
+        }
+        else if (selectedSnake.transform.CompareTag("URH"))
+        {
+            Debug.Log("URH");
+            selectedSnakeGlow = Instantiate(upRightHorizontalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x + 2.785f, selectedSnake.transform.position.y - 0.15f);
+        }
+        else if (selectedSnake.transform.CompareTag("URV"))
+        {
+            Debug.Log("URV");
+            selectedSnakeGlow = Instantiate(upRightHorizontalSnakeGlowPrefab);
+            selectedSnakeGlow.transform.position = new Vector2(selectedSnake.transform.position.x - 0.01f, selectedSnake.transform.position.y + 0.037f);
+        }
+        selectedSnakeGlow.GetComponent<SpriteRenderer>().color = Color.cyan;
+    }
+
     void generateSnakes()
     {
-        GameObject snake = Instantiate(downRightVerticalSnakePrefab);
+        GameObject snake = Instantiate(downRightHorizontalSnakePrefab);
         snake.GetComponent<SpriteRenderer>().color = getRandomColor();
         snake.transform.position = new Vector2(-6.1f, -0.44f);
         snake.name = "Snake";
         snakes[0] = snake;
+        selectedSnakeIndex = 0;
         selectedSnake = snake;
+        OutlineSnake();
 
         GameObject snake1 = Instantiate(downRightVerticalSnakePrefab);
         snake1.GetComponent<SpriteRenderer>().color = getRandomColor();
@@ -106,7 +221,7 @@ public class InitializeSceneScript : MonoBehaviour
             case 3:
                 return Color.blue;
             case 4:
-                return Color.cyan;
+                return Color.white;
             case 5:
                 return Color.yellow;
             case 6:
