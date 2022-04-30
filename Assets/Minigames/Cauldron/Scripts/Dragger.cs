87afdb42b0e0eb0f4a9d4f6e51b63b79d787f2d2
ombahiwal <ombahiwal@gmail.com>
2022-04-30 22:05:37 +0200

    Winning logic demo and new controls

--- Assets/Minigames/Cauldron/Scripts/Dragger.cs
+++ Assets/Minigames/Cauldron/Scripts/Dragger.cs
@@ -1,47 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-public class Dragger : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler
+public class Dragger : MonoBehaviour
 {
     public delegate void DragEndedDelegate(Dragger draggableObject);
-    public DragEndedDelegate dragEndedCallback;
+    public static DragEndedDelegate dragEndedCallback;
     private Vector3 _dragOffset;
     private Camera _cam;
+    public float volume=0.5f;
+
     [SerializeField] private float _speed = 20;
+    [SerializeField] public string name_ing = "default_ingredient";
+    public AudioSource ingSound;
+    private Collider2D _collShape;
+
+    private bool isBeingDragged;
+
+    private static bool SomethingIsBeingDragged;
 
     private RectTransform rectTransform;
     // POinter Events
-    public void OnBeginDrag (PointerEventData eventData) {
-        Debug.Log("Onbegindrag");
-    }
-    public void OnDrag (PointerEventData eventData) {
-            Debug.Log("OnDrag");
-    }
-    public void OnEndDrag (PointerEventData eventData) {
-        Debug.Log("OnEndDrag");
+    // public void OnBeginDrag(PointerEventData eventData) {
+    //     Debug.Log("Onbegindrag");
+    // }
+    // public void OnDrag(PointerEventData eventData) {
+    //     Debug.Log("OnDrag");
+    // }
+    // public void OnEndDrag(PointerEventData eventData) {
+    //     Debug.Log("OnEndDrag");
+    // }
 
-    }
-
-    public void OnPointerDown (PointerEventData eventData) {
-    }
+    // public void OnPointerDown (PointerEventData eventData) {
+    // }
      void Awake(){
         _cam = Camera.main;
-
+        _collShape = GetComponent<Collider2D>();
     }
     private void OnMouseDown() {
-        _dragOffset = transform.position - getMousePos();
+        // _dragOffset = transform.position - getMousePos();
+        Debug.Log("Clicked");
+                // ingSound.PlayOneShot(clip, volume);
+
     }
     void OnMouseDrag()
     {
-        transform.position = Vector3.MoveTowards(transform.position, getMousePos() + _dragOffset, _speed + Time.deltaTime);
+        // transform.position = Vector3.MoveTowards(transform.position, getMousePos() + _dragOffset, _speed + Time.deltaTime);
     }
     private void OnMouseUp() {
         dragEndedCallback?.Invoke(this);
     }
-    Vector3 getMousePos(){
-        var mousePos = _cam.ScreenToWorldPoint(Input.mousePosition + _dragOffset);
-        mousePos.z = 0;
-        return mousePos;
+
+    void Update()
+    {
+        var controlsManager = ControlManager.Instance;
+        
+        // controlsManager.MouseCursorWorldPos;
+
+        if(controlsManager.Controls.Gameplay.Click.triggered && _collShape.OverlapPoint(controlsManager.MouseCursorWorldPos) && (!SomethingIsBeingDragged || isBeingDragged)){
+            isBeingDragged = !isBeingDragged;
+            if(!isBeingDragged){
+                dragEndedCallback?.Invoke(this);
+                SomethingIsBeingDragged = false;
+            }
+            else{
+                SomethingIsBeingDragged = true;
+            }
+        }
+        
+        if(isBeingDragged){
+            transform.position = Vector3.MoveTowards(transform.position, controlsManager.MouseCursorWorldPos + _dragOffset, _speed + Time.deltaTime);
+        }
+        
+        // if(_collShape.OverlapPoint(controlsManager.MouseCursorWorldPos)){
+        //     Debug.Log("HELLLO");
+        // }
+
+        Debug.Log(controlsManager.Controls.Gameplay.Click.triggered);  
+        Debug.Log(controlsManager.Controls.Gameplay.Click.IsPressed());
+
     }
+    
+    
 }
--- Assets/Minigames/Cauldron/Scripts/SnapController.cs
+++ Assets/Minigames/Cauldron/Scripts/SnapController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class SnapController : MonoBehaviour
 {
     public List<Transform> snapPoints;
-    public List<Dragger> draggableObjects;
+    public GameObject couldron;
+    public List<string> droppedIngredients;
+
+    public List<string> winningRecipe; 
+
     public float snapRange = 0.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        foreach(Dragger draggable in draggableObjects){
-            draggable.dragEndedCallback = OnDragEnded;
-        }
+            Dragger.dragEndedCallback = OnDragEnded;
+            winningRecipe = new List<string>{"Carrot", "Eyeball"};
     }
 
    private void OnDragEnded(Dragger draggable){
@@ -20,16 +24,28 @@ public class SnapController : MonoBehaviour
        Transform closestSnapPoint = null;
 
        foreach(Transform snapPoint in snapPoints){
-           float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
+           float currentDistance = Vector2.Distance(draggable.transform.position, snapPoint.position);
            if(closestSnapPoint == null || currentDistance < closestDistance){
                closestSnapPoint = snapPoint;
                closestDistance = currentDistance;
            }
        }
-       
-       if(closestSnapPoint !=null && closestDistance <=snapRange){
-           draggable.transform.localPosition = closestSnapPoint.localPosition;
 
+       if(closestSnapPoint !=null && closestDistance <= snapRange){
+           draggable.transform.localPosition = closestSnapPoint.localPosition;
+            // Snapped draggable properties
+            Debug.Log(draggable.name_ing);            
+            if(winningRecipe.IndexOf(draggable.name_ing) != -1){
+                // Hello 
+                droppedIngredients.Add(draggable.name_ing);
+                if(droppedIngredients.Count == winningRecipe.Count){
+                    // WON
+                    couldron.GetComponent<Renderer>().material.color =  Color.green;
+                }
+            }
+
+            // droppedIngredients.Add(draggable.name_ing);
+            Destroy(draggable.gameObject);
        }
 
    }
