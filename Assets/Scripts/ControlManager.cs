585f415fb641bf7686dc6c3bc4421199a014d66d
Anca N <negulescuanca11@gmail.com>
2022-04-30 18:22:28 +0200

    Comment block

--- Assets/Scripts/ControlManager.cs
+++ Assets/Scripts/ControlManager.cs
@@ -18,22 +18,11 @@ public class ControlManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Controls.Gameplay.Action1.IsPressed()) {
-            // Debug.Log("Action1");
-        }
-        if (Controls.Gameplay.Action2.IsPressed())
-        {
-            // Debug.Log("Action2");
-        }
-        if (Controls.Gameplay.Action3.IsPressed())
-        {
-            // Debug.Log("Action3");
-        }
-        if (Controls.Gameplay.Action3.IsPressed())
-        {
-            // Debug.Log("Action3");
-        }
+        /* if (Controls.Gameplay.Action1.IsPressed()) Debug.Log("Action1");
+        if (Controls.Gameplay.Action2.IsPressed()) Debug.Log("Action2");
+        if (Controls.Gameplay.Action3.IsPressed()) Debug.Log("Action3");
+        if (Controls.Gameplay.Action4.IsPressed()) Debug.Log("Action4");
         var movement = Controls.Gameplay.Movement.ReadValue<Vector2>();
-        if (movement.magnitude > 0) Debug.Log("Movement: " + movement);
+        if (movement.magnitude > 0) Debug.Log("Movement: " + movement); */
     }
 }
